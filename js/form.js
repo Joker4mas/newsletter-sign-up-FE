8cedfc2b27364ab6e92ffd93d677df9efa331b46
joker4mas <samuelogbaje@gmail.com>
2024-01-19 17:20:43 +0100

    fixed the js aspect, it might not be perfect though

--- js/form.js
+++ js/form.js
@@ -1,48 +1,50 @@
 //This is the beginning of the function for validation
 const form = document.querySelector('#newsletter')
-// const mailInput = document.querySelector('email')
 const error = document.querySelector('#email + span.error');
 const Submit = document.querySelector('#subButton');
 const mainContainer = document.querySelector('#signUp');
 const mailInput = document.querySelector('#email');
-
 const userEmail = document.querySelector('#userEmail');
 const successMessage = document.querySelector('.secondContainer');
 const home = document.querySelector('#disMiss');
 
-mailInput.addEventListener('onKeyDown', (e)=>{
+// mailInput.addEventListener('onKeyDown', (e)=>{
 
-});
+// });
 
+// function validateEmail(){
+//     const regex =  /^(?:\d{3}|\(\d{3}\))([-/.])\d{3}\1\d{4}$/;
+//     if (!(mailInput.match(emailCorrectPattern))) {
+//         error.textContent = 'Enter email address';
+//         return false;
+//     }
+// }
 
 function updatedUserEmail(email) {
     userEmail.textContent = email;
 }
 function switchPages(){
-    mainContainer.classList.toggle("hidden")
-    successMessage.classList.toggle("hidden")
+    successMessage.classList.remove('hidden');
+    mainContainer.style.display=  "none";
 }
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    // validateEmail();
     updatedUserEmail(mailInput.value);
+    mailInput.value = "";
     switchPages();
 
 });
 
-function myError(e){
-    error.textContent = "Enter an email address";
-}
-
-{/* <input type="text" onkeypress="myFunction()"> */}
+// function myError(e){
+//     error.textContent = "Enter an email address";
+// }
 
 
-// home.addEventListener('click' , (e) => {
-//     successMessage.classList.add('hidden');
-//     mainContainer.style.display=  "block";
-// }) 
+home.addEventListener('click' , (e) => {
+    successMessage.classList.add('hidden');
+    mainContainer.style.display=  "";
+}) 
 
-// home.addEventListener('click' , (e) => {
-//     successMessage.classList.add('hidden');
-//     mainContainer.style.display=  "block";
-// }) 
